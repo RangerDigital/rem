2af537247169268b6f8f718bfc5faf775ba2bd9a
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2020-02-12 00:38:40 +0100

    Add Flask-CORS

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,11 @@
 import random
 import redis
+from flask_cors import CORS
 from flask import Flask, jsonify, request
 
 
 app = Flask(__name__)
+CORS(app)
 
 if __name__ == "__main__":
     app.config.from_object("config.DevConfig")
